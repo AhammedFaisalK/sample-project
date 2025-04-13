9e9357a164ec3a4fa27d09f1cfdf850a7f10dcb7
ahammedfaisal <ahammedfaisal@tegain.com>
2025-04-13 12:48:49 +0530

    Updated tests.py

--- main_app/tests.py
+++ main_app/tests.py
@@ -3,6 +3,6 @@ from django.test import TestCase
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests that 1 + 1 always equals 2 dhnfjlsdjffk.
         """
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(1 + 5, 6)
